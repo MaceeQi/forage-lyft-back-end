44b5fbda62d5a383daf86979064f47ce408c2b18
Macee Qi <maceeqi@gmail.com>
2024-01-18 13:07:27 -0500

    task4: upgrade Spindler batteries

--- test/test_car.py
+++ test/test_car.py
@@ -47,21 +47,21 @@ class TestNubbinBattery(unittest.TestCase):
 class TestSpindlerBattery(unittest.TestCase):
     def test_battery_should_be_serviced1(self):
         today = datetime.today().date()
-        last_service_date = today.replace(year=today.year - 3)
+        last_service_date = today.replace(year=today.year - 4)
 
         battery = SpindlerBattery(last_service_date, today)
         self.assertTrue(battery.needs_service())
 
     def test_battery_should_be_serviced2(self):
         today = datetime.today().date()
-        last_service_date = today.replace(year=today.year - 20)
+        last_service_date = today.replace(year=today.year - 30)
 
         battery = SpindlerBattery(last_service_date, today)
         self.assertTrue(battery.needs_service())
 
     def test_battery_should_not_be_serviced1(self):
         today = datetime.today().date()
-        last_service_date = today.replace(year=today.year - 2)
+        last_service_date = today.replace(year=today.year - 3)
 
         battery = SpindlerBattery(last_service_date, today)
         self.assertFalse(battery.needs_service())
